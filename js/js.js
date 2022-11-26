102b3562a749eac430c7733b84bcee9dae5de0a2
Gil Sala <gilsala_88@hotmail.com>
2022-11-26 12:10:10 +0100

    Wire buttons and create functions

---
+++ js/js.js
@@ -0,0 +1,106 @@
+const allClear = document.querySelector('[data-all-clear');
+const numbers = document.querySelectorAll('[data-number]');
+const previousDisplay= document.querySelector('[data-previous-number-display]');
+const currentDisplay = document.querySelector('[data-current-number-display]');
+const decimal = document.querySelector('[data-number]');
+const clear = document.querySelector('[data-clear]');
+const equals = document.querySelector('[data-equal]');
+const op = document.querySelectorAll('[data-operator]');
+
+
+let currentNumber='';
+let previousNumber='';
+let operator ='';
+
+
+
+
+allClear.addEventListener('click', ()=>{
+    currentNumber='';
+    previousNumber='';
+    operator=''
+    currentDisplay.innerText = '';
+    previousDisplay.innerText ='';
+});
+
+clear.addEventListener('click', ()=>{
+   currentDisplay.innerText = currentDisplay.innerText.slice(0,-1);
+   currentNumber=currentDisplay.innerText
+   
+});
+
+
+numbers.forEach(item => {
+    item.addEventListener('click', (e) => { 
+    number(currentNumber)
+    currentNumber +=  e.target.textContent;
+    currentDisplay.innerText = currentNumber;
+    
+
+   
+    })
+  });
+
+  function number(num){
+    if(num.length<5)
+ return num
+  }
+
+  op.forEach(item => {
+    item.addEventListener('click', (e)=>{
+    operate();
+    operator =e.target.textContent;
+    previousNumber = currentNumber ;
+    previousDisplay.innerText = previousNumber + ' ' +operator;
+    currentNumber='';
+    currentDisplay.innerText = currentNumber;
+    
+  })
+});
+
+function operate(){
+    
+    if(operator==='+'){
+        const result = Number(currentNumber) + Number(previousNumber);
+        previousNumber='';
+        
+        currentNumber=result;
+        previousDisplay.innerText =previousNumber;
+        currentDisplay.innerText = result;
+    }else if(operator==='-'){
+        const result = Number(previousNumber) -Number(currentNumber);
+        previousNumber='';
+        operator='';
+        currentNumber=result;
+        previousDisplay.innerText =previousNumber;
+        currentDisplay.innerText = result;
+        }else if(operator==='x'){
+        const result = Number(previousNumber) * Number(currentNumber);
+        previousNumber='';
+        operator='';
+        currentNumber=result;
+        previousDisplay.innerText =previousNumber;
+        currentDisplay.innerText = result;
+    
+        }else if(operator==='/'){
+        const result = Number(previousNumber) / Number(currentNumber);
+        previousNumber='';
+        operator='';
+        currentNumber=result;
+        previousDisplay.innerText =previousNumber;
+        currentDisplay.innerText = result;
+    
+        }else if(operator==='%'){
+        const result = Number(previousNumber) % Number(currentNumber);
+        previousNumber='';
+        operator='';
+        currentNumber=result;
+        previousDisplay.innerText =previousNumber;
+        currentDisplay.innerText = result;
+        }
+
+}
+
+
+
+equals.addEventListener('click', operate);
